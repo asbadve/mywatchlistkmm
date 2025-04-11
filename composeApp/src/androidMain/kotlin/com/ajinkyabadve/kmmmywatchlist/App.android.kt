a1b8d28a8e46f4d4b7eed2db7213664954373e8f
ajinkyabadve <asbadve@gmail.com>
2025-04-11 18:08:29 +0530

    feat(improvement to app screen): fixed the status bar issue in light mode android

--- composeApp/src/androidMain/kotlin/com/ajinkyabadve/kmmmywatchlist/App.android.kt
+++ composeApp/src/androidMain/kotlin/com/ajinkyabadve/kmmmywatchlist/App.android.kt
@@ -7,15 +7,13 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.toComposeRect
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 import androidx.window.layout.WindowMetricsCalculator
 import com.ajinkyabadve.kmmmywatchlist.core.WindowSize
 
@@ -33,15 +31,9 @@ class AndroidApp : Application() {
 @OptIn(ExperimentalMaterial3Api::class)
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            val view = LocalView.current
-            if (!view.isInEditMode) {
-                SideEffect {
-                    val window = (view.context as Activity).window
-                    WindowCompat.setDecorFitsSystemWindows(window, false)
-                }
-            }
             App()
         }
     }
